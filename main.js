b62787e89824937159d0a77cc7a064be2997c793
Erika Faustino <dossantos.erika08@gmail.com>
2020-06-20 21:44:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 ﻿const quotes = [
+   {
+     "quote": "Quando me atrevo a ser poderosa, a usar minha força ao serviço da minha visão, o medo que sinto se torna cada vez menos importante.",
+     "author": "Audre Lorde",
+   },
   {
         "quote": "Numa sociedade racista não basta não ser racista. É necessário ser antiracista.",
         "author": "Angela Davis",
@@ -20,7 +24,27 @@
     {
         "quote":"Feminismo é uma noção radical de que as mulheres são seres humanos.",
         "author":"Marie Shear"
-    }
+    },
+    {
+        "quote":"Você tem que agir como se fosse possível transformar radicalmente o mundo. E você tem que fazer isso o tempo todo.",
+        "author":"Angela Davis",
+    },
+   {
+        "quote":"qual é a maior lição que uma mulher pode aprender? Que desde o primeiro dia, ela sempre teve tudo o que precisa dentro de si mesma. foi o mundo que a convenceu que ela não tinha.",
+        "author":"Rupi Kaur",
+    },
+   {
+        "quote":"Os homens temem que as mulheres riam deles. As mulheres temem que os homens as matem.",
+        "author":"Margareth Antwood",
+    },
+   {
+        "quote":"Pela maior parte da História, 'anônimo' foi uma mulher.",
+        "author":"Virgínia Woolf",
+    },
+   {  
+     "quote": "Honrar a nós mesmas, amar nossos corpos, é uma fase avançada na construção de uma autoestima saudável.",
+      "author": "Bell Hooks",
+   }
 ];
 
 function randomQuotesGenerator() {
